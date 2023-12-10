93c2b1865241eace9cc3ca25984acb82010b9a69
Openiuk <openyusha@gmail.com>
2023-12-10 20:36:23 +0100

    test push 2

--- test.py
+++ test.py
@@ -1,21 +1,76 @@
-class Animal:
-    color = "white"
+import argparse
+from collections import defaultdict
 
-    def __init__(self, nickname, weight):
-        self.nickname = nickname
-        self.weight = weight
 
-    def say(self):
-        pass
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (KeyError, ValueError, IndexError) as e:
+            print(f'Error: {e}')
+    return wrapper
 
-    def change_weight(self, weight):
-        self.weight = weight
 
-    def change_color(self):
-        Animal.color = 'red'
+def hello_func(*args, **kwargs):
+    print('How can I help you?')
 
-dog = Animal('jack', 33)
-cat = Animal('Vasya', 6)
-print(dog.color)
-cat.change_color()
-print(cat.color)
+@input_error
+def add_func(args, num_dict):
+    num_dict[args.name] = args.number
+    return num_dict
+
+@input_error
+def change_func(args, num_dict):
+    if args.name in num_dict:
+        num_dict[args.name] = args.number
+    else:
+        raise KeyError(f'No such name in the dictionary!')
+
+@input_error
+def phone_func(args, num_dict):
+    result = num_dict.get(args.name, None)
+    if result is None:
+        raise KeyError('No such name in the dictionary.')
+    print(f'For the contact {args.name}, the number is {result}')
+
+@input_error
+def show_func(*args, **kwargs):
+    print(kwargs['num_dict'])
+
+
+COMAND_DICT = {
+    'hello': hello_func,
+    'add': add_func,
+    'change': change_func,
+    'phone': phone_func,
+    'show_all': show_func
+}
+
+
+@input_error
+def handle_func(args, num_dict):
+    if args.command in COMAND_DICT:
+        return COMAND_DICT[args.command](args, num_dict=num_dict)
+    else:
+        raise KeyError('Unknown command')
+
+
+def main():
+    num_dict = defaultdict(str)
+    while True:
+        command = input('Enter command: ')
+        if command.lower() in ['good bye', 'close', 'exit']:
+            print('Good bye!')
+            break
+        command_parts = command.lower().split()
+        parser = argparse.ArgumentParser(description='Assistant bot program.')
+        parser.add_argument('command', choices=COMAND_DICT.keys(), help='Available commands')
+        parser.add_argument('name', nargs='?', default='', help='Contact name')
+        parser.add_argument('number', nargs='?', default='', help='Contact number')
+        args = parser.parse_args(command_parts)
+        result = handle_func(args, num_dict)
+        if result is not None:
+            print(result)
+
+if __name__ == '__main__':
+    main()
--- test2.py
+++ test2.py
@@ -1,23 +1,83 @@
 import argparse
+from collections import defaultdict
 
+
+def input_error(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (KeyError, ValueError, IndexError) as e:
+            print(f'Error: {e}')
+    return wrapper
+
+
+def hello_func(*args, **kwargs):
+    print('How can I help you?')
+
+@input_error
 def add_func(args, num_dict):
     num_dict[args.name] = args.number
-    print(num_dict)
     return num_dict
 
+@input_error
+def change_func(args, num_dict):
+    if args.name in num_dict:
+        num_dict[args.name] = args.number
+    else:
+        raise KeyError(f'No such name in the dictionary!')
+
+@input_error
+def phone_func(args, num_dict):
+    result = num_dict.get(args.name, None)
+    if result is None:
+        raise KeyError('No such name in the dictionary.')
+    print(f'For the contact {args.name}, the number is {result}')
+
+@input_error
+def show_func(*args, **kwargs):
+    print(kwargs['num_dict'])
+
+
+COMAND_DICT = {
+    'hello': hello_func,
+    'add': add_func,
+    'change': change_func,
+    'phone': phone_func,
+    'show_all': show_func
+}
+
+
+@input_error
+def handle_func(args, num_dict):
+    if args.command in COMAND_DICT:
+        return COMAND_DICT[args.command](args, num_dict=num_dict)
+    else:
+        raise KeyError('Unknown command')
+
+
 def main():
-    num_dict = {}  # Создаем словарь для номеров телефонов
-    parser = argparse.ArgumentParser(description='Программа бот ассистент.')
-    subparsers = parser.add_subparsers(help='Доступные команды')
+    num_dict = defaultdict(str)
+    while True:
+        command = input('Enter command: ')
+        if command.lower() in ['good bye', 'close', 'exit']:
+            print('Good bye!')
+            break
 
-    add_parser = subparsers.add_parser('add', help='Парсер функции "add"')
-    add_parser.add_argument('name', help='Имя контакта')
-    add_parser.add_argument('number', help='Номер контакта')
-    add_parser.set_defaults(func=lambda args: add_func(args, num_dict))
+        command_parts = command.lower().split()
+        command_name = command_parts[0]
 
-    args = parser.parse_args()
-    args.func(args)
+        if command_name in COMAND_DICT:
+            parser = argparse.ArgumentParser(description='Assistant bot program.')
+            parser.add_argument('command', choices=COMAND_DICT.keys(), help='Available commands')
+            parser.add_argument('name', nargs='?', default='', help='Contact name')
+            parser.add_argument('number', nargs='?', default='', help='Contact number')
+            args = parser.parse_args(command_parts)
+            result = handle_func(args, num_dict)
+            if result is not None:
+                print(result)
+        else:
+            print('Invalid command. Please try again.')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
